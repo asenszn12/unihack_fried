70be959a96b71ef0b8646cbf0906093bad4f6e19
Aadhi Senthil Kumar <z5584552@ad.unsw.edu.au>
2025-03-16 00:04:13 +1100

    made main file, in addition to importing emission calc file and flutter package

--- main.dart
+++ main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'emissions_calculator.dart';
 
+void main() {
+  runApp(const EmissionsCalculatorApp());
+}
+
+class EmissionsCalculatorApp extends StatelessWidget {
+  const EmissionsCalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Greena',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        brightness: Brightness.light,
+        fontFamily: 'Roboto',
+      ),
+      home: const EmissionsCalculator(),
+    );
+  }
+}
