a60b00b7f436826aff689b02cbcc53b4b7f3b030
Hamza Bilal <hamzabilal@Hamzas-MacBook-Air.local>
2025-03-15 20:01:07 +1100

    initialised class for cacluator

--- emissions.dart
+++ emissions.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/material.dart';
+import 'food_item.dart';
 
+class EmissionsCalculator extends StatefulWidget {
+  const EmissionsCalculator({super.key});
+
+  @override
+  _EmissionsCalculatorState createState() => _EmissionsCalculatorState();
+}
